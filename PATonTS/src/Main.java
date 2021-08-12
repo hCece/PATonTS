f3e4b46dada960c31a55ea323700244bb8408dbe
hCece <72106079+hCece@users.noreply.github.com>
2021-08-12 11:51:35 +0200

    Add files via upload

---
+++ PATonTS/src/Main.java
@@ -0,0 +1,42 @@
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import TeleBot.TeleBot;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+/**
+ * This is a simple Main class that starts two processes: TeleBot and the JAVAfx project.  
+ * I used some simple Threads implementation two run both processes at the same time.
+ * 
+ */
+public class Main extends Application implements Runnable {
+	TeleBot teleBot = new TeleBot();
+	@Override	
+	public void start(Stage primaryStage) throws Exception {
+		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/mainScene.fxml"));
+		loader.setController(new Controller.MainScene("U_02"));
+		/*
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/login.fxml"));
+		loader.setController(new Controller.LogIn());
+		*/
+		Scene logInScene = loader.load();
+		primaryStage.setScene(logInScene);
+		primaryStage.show();
+	}
+	 
+	public static void main(String[] args) {
+		Main main = new Main();
+	    Thread thread = new Thread(main);
+	    thread.start();
+    	launch(args);
+    	main.teleBot.loop = false;
+
+	}
+	public void run() {
+    	teleBot.start();	   
+	}
+	
+}
